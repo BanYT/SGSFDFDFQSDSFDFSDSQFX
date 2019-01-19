8c53ce307f784bcdb173000c788025f31809a8dd
BanYT <44928120+BanYT@users.noreply.github.com>
2019-01-19 21:16:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 Discord = require("discord.js");
 const client = new Discord.Client();
+require('./util/cmdloader.js')(client);//requires the command loader
 let token = process.env.token;
-
+let prefix = "!";
 
 
 client.on('ready', () => {
 console.log('IM READY !');
 });
 
+client.on('message', message => {
+ if (message.author.bot) return;
+ if (!message.content.startsWith(prefix)) return;
 
-});     
+  let command = message.content.split(" ")[0];
+  cmd = command.slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
+
+  
+//command handler
+let commandfile = client.commands.get(cmd);
+  let alias = client.aliases.get(cmd);
+
+  if(commandfile){
+      commandfile.run(client,message,args);
+  }
+  if(alias){
+      alias.run(client,message,args);
+  }
+        
           
         
 });
