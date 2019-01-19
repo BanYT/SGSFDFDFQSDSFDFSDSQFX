669a0191f9ef2b5a488b3eff28de9e32414e36c3
BanYT <44928120+BanYT@users.noreply.github.com>
2019-01-19 21:17:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 Discord = require("discord.js");
 const client = new Discord.Client();
-require('./util/cmdloader.js')(client);//requires the command loader
 let token = process.env.token;
-let prefix = "!";
+
 
 
 client.on('ready', () => {
