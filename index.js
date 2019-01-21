bd45315707ee3e35e7ed74f30036efb246240d97
BanYT <44928120+BanYT@users.noreply.github.com>
2019-01-21 17:29:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,36 @@
-Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-let token = process.env.token;
-let prefix = "!";
-
-
 
 client.on('ready', () => {
-console.log('IM READY !');
-});
+  client.user.setGame(' https://discord.gg/hXJZhqa ', 'https://www.twitch.tv/lucasdavid913/')
+})   
 
-client.on('message', message => {
- if (message.author.bot) return;
- if (!message.content.startsWith(prefix)) return;
+client.on('message', message => {    
+    if(message.content.startsWith('*mass')) {
+    if(message.author.id === "409351580551479308" ||
+message.author.id === "409351580551479308"){
+       let args = message.content.split(" ").slice(1);         
+       var argresult = args.join(" ")      
+const argsresult = args.join(" ")         
+       let reason = args.join(" ")                   
+    if(!args[1]) {  }  
+    if(args[1]) { 
+message.channel.guild.members.forEach(member => {{ 
+member.send(reason)
+message.delete() }})}}} });
 
-  let command = message.content.split(" ")[0];
-  cmd = command.slice(prefix.length);
+client.on('message', message =>{
   let args = message.content.split(" ").slice(1);
 
-  
-//command handler
-let commandfile = client.commands.get(cmd);
-  let alias = client.aliases.get(cmd);
-
-  if(commandfile){
-      commandfile.run(client,message,args);
-  }
-  if(alias){
-      alias.run(client,message,args);
-  }
- 	client.on('guildMemberAdd', member => {
-    member.guild.channels.get('536279384676499462').setName(`Total Users: ${member.guild.memberCount}`);
-});
-
-client.on('guildMemberRemove', member => {
-    member.guild.channels.get('536279384676499462').setName(`Total Users: ${member.guild.memberCount}`);
-});       
-client.on('messageDelete', async (message) => {
-    const logs = message.guild.channels.find('name', 'bot-log');
-    if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !logs) {
-        await message.guild.createChannel('bot-log', 'text');
-    }
-    if (!logs) {
-        return console.log('The logs channel does not exist and cannot be created')
-    }
-    const entry = await message.guild.fetchAuditLogs({
-        type: 'MESSAGE_DELETE'
-    }).then(audit => audit.entries.first())
-    let user;
-    if (entry.extra.channel.id === message.channel.id && (entry.target.id === message.author.id) && (entry.createdTimestamp > (Date.now() - 5000)) && (entry.extra.count >= 1)) {
-        user = entry.executor.username
-    } else {
-        user = message.author
-    }
-    const logembed = new Discord.RichEmbed()
-        //.setTitle('Message Deleted')
-        .setAuthor(user.tag, message.author.displayAvatarURL)
-        .addField(`**Message sent by ${message.author.username}> deleted in ${message.channel.name}**\n\n`, message.content)
-        .setColor(message.guild.member(client.user).displayHexColor)
-        .setFooter(`<#${message.channel.id}>`)
-        .setTimestamp()
-    //console.log(entry)
-    logs.send(logembed);
-})          
+  if ( message.content.startsWith("https://discord.gg")) {
+    message.delete()
+    let embed = new Discord.RichEmbed()
+            .setTitle('No Invite Link')
+            .setColor('RANDOM')
+            .setFooter('No invite link!')
+            .setTimestamp()
         
-});
-     
-  client.login(process.env.token);
+        message.channel.send(embed);
+  }
+
+});  
